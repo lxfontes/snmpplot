cda3ce1dd10d274d146b7a74fcf8017c7dbb77d1
Lucas Fontes <lucas@l3f.org>
2010-11-08 21:37:49 -0500

    first commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+# A sample Gemfile
+source "http://rubygems.org"
+
+gem 'snmp'
+gem 'sinatra'
+gem 'json'
---
+++ snmp-plot.rb
@@ -0,0 +1,36 @@
+require 'sinatra'
+require 'snmp'
+require 'erubis'
+require 'json'
+
+
+class SNMPPlot < Sinatra::Base
+  include SNMP
+
+  enable :static
+  set :public, File.dirname( __FILE__ ) + '/public' 
+
+  get '/s' do
+    host = params[:host]
+    path = params[:path]
+    values = nil
+    oidlist = []
+    path.each do |p|
+      oidlist << ObjectId.new(p)
+    end
+    SNMP::Manager.open(:Host => host) do |snmp|
+          values = snmp.get_value(oidlist)
+    end
+    puts "VV #{values.to_json}"
+    return values.to_json if values
+  end
+
+  get '/' do
+    erubis :index
+  end
+
+
+end
+
+SNMPPlot.run! :host => '0.0.0.0', :port => 9091
+
